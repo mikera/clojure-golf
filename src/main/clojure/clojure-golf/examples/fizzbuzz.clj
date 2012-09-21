157a88ffdea65d0fe72fe3875ab87da403b76228
mikera <Mike@Mike-VAIO>
2012-09-21 15:45:38 +0800

    Minor improvements

--- src/main/clojure/clojure-golf/examples/fizzbuzz.clj
+++ src/main/clojure/clojure-golf/examples/fizzbuzz.clj
@@ -1,3 +1,7 @@
 ;; Play the famous "FizzBuzz" game for the first 100 numbers
 
+;; v1 - 81 charcaters
 (for[n(range 1 101)t[(mod n 3)]z[{0"Fizz"}]]({0(str(z t)'Buzz)}(mod n 5)(z t n)))
+
+;; v2 - 80 characters
+(map #(let[t(mod% 3)z{0"Fizz"}]({0(str(z t)'Buzz)}(mod% 5)(z t%)))(range 1 101))
--- src/main/clojure/clojure-golf/tricks.clj
+++ src/main/clojure/clojure-golf/tricks.clj
@@ -5,19 +5,19 @@
 ;; General / basic tricks
 
 ;; whitespace is optional in clojure unless needed to delimit symbols
-;; spaces around all types of parentheses and delimiters can be removed
+;; spaces around all types of parentheses etc. can be removed
 (for [i (range 10)] {:key i :value "foo"})
 (for[i(range 10)]{:key i :value"foo"})
-
 
 ;; ===============================================
 ;; Strings
 
-;; strings can often be used directly as a sequence of characters, without needing "seq"
+;; strings can often be used directly as a sequence of characters
+;; - without needing "seq"
 (reverse (seq "Abracadabra"))
 (reverse "Abracadabra")
 
-;; strings used together with maps are useful to generate longer sequences
+;; strings used with maps are useful to generate long sequences
 (let[a "Eggs"b"Jelly"c"Toast"][a b c a a b b c c a a a b b b c c c])
 (map{\a"Eggs"\b"Jelly"\c"Toast"}"abcaabbccaaabbbccc")
 
@@ -47,7 +47,8 @@
 (filter #(= "Foo" %) ["Foo" "Bar" "Baz"])
 (filter #{"Foo"} ["Foo" "Bar" "Baz"])
 
-;; hashmaps are equivalent to and shorter than sequences of pairs when used as a sequence
+;; hashmaps are equivalent to and shorter than sequences of pairs 
+;; when used as a sequence
 (seq [["A"1]["B"2]["C"3]])
 (seq {"A"1"B"2"C"3})
 
@@ -77,16 +78,18 @@
 ;; ======================================================
 ;; Advanced tricks
 
-;; adding a single-element vector to a for loop can be shorter than a let
+;; adding a single-element vector to a for loop can be shorter
+;; when compared to repeating a literal or a let
 (for [i (range 10)] [(str "A long-string needed twice" i) "A long-string needed twice"])
 (for [i (range 10)] (let [s "A long-string needed twice"] [(str s i) s]))
 (for [i (range 10) s ["A long-string needed twice"]] [(str s i) s])
 
-;; if they are goimg to be converted to strings, using symbols can be shorter than quoted text
+;; if they are goimg to be converted to strings, using symbols 
+;; can be shorter than quoted text
 (map #(str "foo" %) ["A" "B" "C"])
 (map #(str "foo" %) ['A 'B 'C])
 
-;; if you use a function repeatedly, it is often better to alias it
+;; if you use a function repeatedly, it may be better to alias it
 (for [i (range 100)] [(mod i 3) (mod i 5) (mod i 7) (mod i 9)])
 (for [i (range 100)m[mod]] [(m i 3) (m i 5) (m i 7) (m i 9)])
 (for [i (range 100)m[#(mod i %)]] [(m 3) (m 5) (m 7) (m 9)])
